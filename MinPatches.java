d5fdd93f50a293ae32fd5b2365ea67a13d3e4b74
shuoshuo70 <724722392@qq.com>
2017-06-09 12:59:10 +0800

    lc330

--- MinPatches.java
+++ MinPatches.java
@@ -7,7 +7,8 @@ public class MinPatches {
 		System.out.println(solution.minPatches(nums, 5));
 	}
 	public int minPatches(int[] nums, int n) {
-        int sum = 0, i = 0, add = 0;
+        long sum = 0;
+        int i = 0, add = 0;
         while(sum < n) {
             if(i < nums.length && (nums[i] == 1 || nums[i] - 1 <= sum)) {
                 sum += nums[i++];

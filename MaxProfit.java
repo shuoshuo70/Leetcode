d379c64ccf90b5dc2dd8c9c99eef72db6be5710a
shuoshuo70 <724722392@qq.com>
2017-07-21 23:06:54 +0800

    lc309

--- MaxProfit.java
+++ MaxProfit.java
@@ -9,16 +9,16 @@ public class MaxProfit {
 	
 	public int maxProfit(int[] prices) {
         if(prices.length <= 1) return 0;
-        int[] buy = new int[prices.length];
-        int[] sell = new int[prices.length];
-        buy[0] = -prices[0];
-        buy[1] = Math.max(buy[0], -prices[1]);
-        sell[1] = Math.max(0, buy[0] + prices[1]);
-        for(int i=2; i<prices.length; i++) {
-            buy[i] = Math.max(buy[i-1], sell[i-2] - prices[i]);
-            sell[i] = Math.max(sell[i-1], buy[i-1] + prices[i]);
+        int b0 = -prices[0], b1 = b0, b2 = 0, s0 = 0, s1 = 0, s2 = 0;
+        for(int i=1; i<prices.length; i++) {
+            b2 = Math.max(b1, s0 - prices[i]);
+            s2 = Math.max(s1, b1 + prices[i]);
+            b0 = b1;
+            b1 = b2;
+            s0 = s1;
+            s1= s2;
         }
         
-        return sell[prices.length-1];
+        return s2;
     }
 }

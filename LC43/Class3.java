30bf4b58a0c3c561ba92848486b9dfcd7f1b32f5
shuoshuo70 <724722392@qq.com>
2017-07-31 13:05:42 +0800

    contest43

---
+++ LC43/Class3.java
@@ -0,0 +1,29 @@
+package LC43;
+
+public class Class3 {
+public String predictPartyVictory(String senate) {
+        
+        Queue<Integer> r = new LinkedList<>();
+        Queue<Integer> d = new LinkedList<>();
+        
+        //store index of r and d
+        for(int i=0; i<senate.length(); i++) {
+            if(senate.charAt(i) == 'R') r.off
+                er(i);
+            else d.offer(i);
+        }
+        
+        int n = senate.length();
+        
+        //poll element, and stay live one offer in the end and index + n, for the next circle, and by using n, the last one and the first one is connected
+        while(!r.isEmpty() && !d.isEmpty()) {
+            int rNum = r.poll(), dNum = d.poll();
+            
+            //smaller one lives
+            if(rNum < dNum) r.offer(rNum + n);
+            else d.offer(dNum + n);
+        }
+        
+        return r.isEmpty() ? "Dire" : "Radiant";
+    }
+}
